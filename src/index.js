c1a66a80c3541f145b951073872626d7eac02d69
mq2y4vns7x-crypto <mq2y4vns7x@privaterelay.appleid.com>
2026-01-02 19:15:42 -0500

    Update and rename NebulaEdgeApp.js to src/index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import NebulaEdgeApp from './NebulaEdgeApp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <NebulaEdgeApp />
+  </React.StrictMode>
+);
