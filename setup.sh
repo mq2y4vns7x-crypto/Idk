61f2d804ec27ad18be1871b50976ca251f06f7df
mq2y4vns7x-crypto <mq2y4vns7x@privaterelay.appleid.com>
2026-01-03 08:46:06 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,1 @@
-npx expo install expo-linear-gradient lucide-react-native react-native-reanimated
-
+npx expo install expo-linear-gradient lucide-react-native react-native-reanimated react-native-safe-area-context expo-blur
